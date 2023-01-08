e75bc1af6a2dc0aceb28909af700c30dc40cdb2c
Mohd_Rizwan <rizuart23@gmail.com>
2023-01-08 23:32:52 +0530

    rizwan

--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -7,7 +7,9 @@ const header = () => {
             <h2>Devolp<span>er</span></h2>
         </div>
         <div className='header__right'>
-            
+            <div className='rizwan'>
+              
+            </div>
         </div>
     </div>
   )
