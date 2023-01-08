47814cd447a0ea8d55dbfef79a4da73a6d90c6d0
Mohd_Rizwan <rizuart23@gmail.com>
2023-01-08 23:32:11 +0530

    rizwan

---
+++ src/component/Header/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div className='header'>
+        <div className='header__right'>
+            <h2>Devolp<span>er</span></h2>
+        </div>
+        <div className='header__right'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default header
