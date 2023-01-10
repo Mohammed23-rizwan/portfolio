bdcb57d1f36964034b20ab76e70cdddd792db9eb
Mohd_Rizwan <rizuart23@gmail.com>
2023-01-10 23:28:43 +0530

    today finished

--- src/component/Top content/Topcontent.js
+++ src/component/Top content/Topcontent.js
@@ -4,15 +4,17 @@ import './Topcontent.css';
 
 const Topcontent = () => {
   return (
+    <div className='top_container'>
     <div className='top__container'>
         <h2>Mr.Mohammed Rizwan</h2>
         <p>I'm a Professional web and app developer</p>
         <a href='#'>
-          <button>Download CV</button>
+          <button className='topcontent_downloadbutton'>Download CV</button>
         </a>
         <Link to='project' smooth={true} duration={500}>
-          <button>My Work</button>
+          <button className='topcontent_myworkbutton'>My Work</button>
         </Link>
+    </div>
     </div>
   )
 }
